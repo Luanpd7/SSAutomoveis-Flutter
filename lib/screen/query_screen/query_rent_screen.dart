8fa0c55c04600f1ab8c958d607920279a93a67fc
Luan Pereira <luan13@gmail.com>
2024-07-14 23:59:31 -0300

    update

--- lib/screen/query_screen/query_rent_screen.dart
+++ lib/screen/query_screen/query_rent_screen.dart
@@ -10,10 +10,6 @@ import '../../provider/pdf_provider.dart';
 import '../../provider/rent_provider.dart';
 import '../pdf_screen.dart';
 
-///[QueryManagerScreen] é a tela resposável pela consulta
-///geral de um gerente, sendo passado o cliente pelo construtor
-
-// ignore: must_be_immutable
 class QueryRentScreen extends StatelessWidget {
   QueryRentScreen({required this.rent});
 
@@ -29,79 +25,91 @@ class QueryRentScreen extends StatelessWidget {
     return Scaffold(
       resizeToAvoidBottomInset: false,
       appBar: AppBar(
-        title:
-            Text('Aluguel'),
+        title: Text('Aluguel'),
         backgroundColor: Theme.of(context).primaryColor,
       ),
       backgroundColor: Theme.of(context).canvasColor,
       body: Consumer<PDFProvider>(
-        builder: (BuildContext context, pdfProvider, Widget? child) {  
-        return Consumer<RentProvider>(
-          builder: (context, managerState, _) {
-            managerState.select();
-            return Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Expanded(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        QueryItem(
-                          title: 'Cliente',
-                          subtitle: rent.clientId.toString(),
-                        ),
-                        QueryItem(title: 'Data Início', subtitle: DateFormat('dd/MM/yyyy').format(rent.dataInicio)),
-                         QueryItem(
-                            title: 'Data de Registro',
-                            subtitle:
-                                _formatDateToBrasilia(rent.dataRegistro)),
-                        FormButton(
-                          labelButton: 'Gerar Comprovante',
-                          onPressed: () async {
-              final pdfFile = await pdfProvider.generatePDF(rent);
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => PDFViewerPage(pdfFile: pdfFile),
+        builder: (BuildContext context, pdfProvider, Widget? child) {
+          return Consumer<RentProvider>(
+            builder: (context, managerState, _) {
+              managerState.select();
+              return Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Expanded(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Expanded(
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              children: [
+                                QueryItem(
+                                  title: 'Cliente',
+                                  subtitle: rent.clientId.toString(),
+                                ),
+                                QueryItem(
+                                  title: 'Data Início',
+                                  subtitle: DateFormat('dd/MM/yyyy').format(rent.dataInicio),
+                                ),
+                                QueryItem(
+                                  title: 'Data de Registro',
+                                  subtitle: _formatDateToBrasilia(rent.dataRegistro),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Expanded(
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              children: [
+                                QueryItem(
+                                  title: 'Veículo',
+                                  subtitle: rent.vehicleId.toString(),
+                                ),
+                                QueryItem(
+                                  title: 'Data Devolução',
+                                  subtitle: DateFormat('dd/MM/yyyy').format(rent.dataFim),
+                                ),
+                                QueryItem(
+                                  title: 'Comissão',
+                                  subtitle: '${rent.comissao}',
+                                ),
+                                QueryItemIcone(
+                                  icon: Icons.edit,
+                                  onTap: () async {
+                                    // Navegar para a tela de edição se necessário
+                                  },
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: 16.0), // Adicionar espaço entre o conteúdo e o botão
+                    FormButton(
+                      labelButton: 'Gerar Comprovante',
+                      onPressed: () async {
+                        final pdfFile = await pdfProvider.generatePDF(rent);
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => PDFViewerPage(pdfFile: pdfFile),
+                          ),
+                        );
+                      },
+                    //  fullWidth: true, // Prop customizada para esticar o botão
+                    ),
+                  ],
                 ),
               );
-            },),
-                        
-                      ],
-                    ),
-                  ),
-                  Expanded(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        QueryItem(title: 'veiculo', subtitle: rent.vehicleId.toString()),
-                        QueryItem(title: 'Data Devolução', subtitle:  DateFormat('dd/MM/yyyy').format(rent.dataFim)),
-                        QueryItem(
-                            title: 'Comissão', subtitle: '${rent.comissao}'),
-                        QueryItemIcone(
-                          icon: Icons.edit,
-                          onTap: () async {
-                          /*  await Navigator.of(context).pushNamed(
-                              AppRoute.updateManager,
-                              arguments: manager,
-                            );*/
-        
-                          }
-                        ),
-                      ],
-                  
-                    ),
-                  ),
-                 
-                  
-                ],
-              ),
-            );
-          },
-        );
-        }
+            },
+          );
+        },
       ),
     );
   }
